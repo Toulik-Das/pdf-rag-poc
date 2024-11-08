d218410392cf8fe1a4efff0dd52995d45f98ab2d
Toulik Das <dastoulik12@gmail.com>
2024-11-08 23:17:35 +0530

    Update processing.py

--- utils/processing.py
+++ utils/processing.py
@@ -5,12 +5,12 @@ import google.generativeai as genai
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS, Pinecone
+from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from typing import List, Generator
-#from pinecone import Pinecone
+from pinecone import Pinecone
 
 # Function to initialize Pinecone vectorstore for knowledge retrieval
 def initialize_pinecone_vectorstore(PINECONE_API_KEY: str):
